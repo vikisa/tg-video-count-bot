31aa304f7abbb719143604d72b83c9b7493c76b0
vikisa <zelikovich.vika@gmail.com>
2025-04-12 23:40:43 +0300

    fix

--- handlers/add_members.py
+++ handlers/add_members.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update, User
 from telegram.ext import CallbackContext
 from db.queries import get_or_create_member, get_marathon_id_and_start, add_member_to_marathon
 
@@ -19,18 +19,18 @@ def add_members_command(update: Update, context: CallbackContext):
   added = []
 
   for entity in mentions:
-    if entity.type != "mention":
+    if entity.type not in ("mention", "text_mention"):
       continue
 
-    username = update.effective_message.text[entity.offset : entity.offset + entity.length]
     user = update.effective_message.parse_entities().get(entity)
 
-    if not user:
-      continue  # возможно, не удалось извлечь info
+    if not isinstance(user, User):
+      continue
 
     tg_id = user.id
+    username = user.username or "без_ника"
     member_id = get_or_create_member(tg_id, username)
     add_member_to_marathon(member_id, marathon_id, start_date)
-    added.append(username)
+    added.append("@" + username)
 
   update.effective_message.reply_text(f"Добавлены в марафон {marathon_name}: {', '.join(added)}")

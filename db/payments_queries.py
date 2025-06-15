0b5f60974f7aa48ceaa763ae0fd7a59adaaf5dc3
vikisa <zelikovich.vika@gmail.com>
2025-06-15 15:15:26 +0300

    add payment

---
+++ db/payments_queries.py
@@ -0,0 +1,13 @@
+from db.connect import get_conn
+
+def insert_payment(marathon_id: int, member_id: int, payment: int):
+  with get_conn() as conn:
+    with conn.cursor() as cur:
+      cur.execute(
+        """
+        INSERT INTO payments (marathon_id, member_id, payment)
+        VALUES (%s, %s, %s);
+        """,
+        (marathon_id, member_id, payment)
+      )
+    conn.commit()
--- handlers/add_payment.py
+++ handlers/add_payment.py
@@ -1,2 +1,33 @@
-def add_payment_command():
-  return
+from telegram import Update
+from telegram.ext import CallbackContext
+from db.member_queries import get_member_id_by_username
+from db.payments_queries import insert_payment
+from utils.consts import ADMIN_TG_ID
+
+def add_payment_command(update: Update, context: CallbackContext):
+  user = update.effective_user
+  chat_type = update.effective_chat.type
+
+  if chat_type != "private" or user.id != ADMIN_TG_ID:
+    update.message.reply_text("Ничего не выйдет")
+    return
+
+  if len(context.args) != 3:
+    update.message.reply_text("❌ Использование: /add_payment <marathon_id> <tg_id> <amount>")
+    return
+
+  try:
+    marathon_id = int(context.args[0])
+    tg_id = context.args[1].replace('@', '')
+    amount = int(context.args[2])
+  except ValueError:
+    update.message.reply_text("Что-то не так с аргументами")
+    return
+
+  member_id = get_member_id_by_username(tg_id)
+  if not member_id:
+    update.message.reply_text("Участник не найден")
+    return
+
+  insert_payment(marathon_id=marathon_id, member_id=member_id, payment=amount)
+  update.message.reply_text(f"✅ Платёж на сумму {amount}₽ добавлен для участника {tg_id}.")

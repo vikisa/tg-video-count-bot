973b4f0d0cdb37b117a8799ddc2469ccf0b79113
vikisa <zelikovich.vika@gmail.com>
2025-06-16 15:43:14 +0300

    f

--- handlers/missed_days.py
+++ handlers/missed_days.py
@@ -58,7 +58,7 @@ def calculate_total_rewards(distribution, members):
     missed_usernames = set(day["missed_lines"])  # например: {'@vika', '@masha'}
     print('missed_usernames',missed_usernames)
     for m in members:
-      username = m["username"].lstrip("@") if m["username"] else None
+      username = f"@{m['username']}" if m["username"] else None
       print('username',username)
       if username and username not in missed_usernames:
         rewards_by_user[username] += day["per_person_payment"]
